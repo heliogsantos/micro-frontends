a8c2658a46e1b5721472ed2b780d29f4050444e4
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-07-13 19:41:29 -0300

    I created header

--- keep-app/src/app/app.module.ts
+++ keep-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
