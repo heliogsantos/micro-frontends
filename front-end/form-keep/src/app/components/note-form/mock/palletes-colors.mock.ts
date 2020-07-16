ee9c793d836df8b52f9635c9c640140db79fe074
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-07-16 18:16:35 -0300

    I created  paste mock and file colorPaletteBallsMock

---
+++ front-end/form-keep/src/app/components/note-form/mock/palletes-colors.mock.ts
@@ -0,0 +1,51 @@
+export const colorPaletteBallsMock = [
+  {
+    className: 'bg-default',
+    checkedColor: true,
+    bgColor: '#ffffffff',
+  },{
+    className: 'bg-red',
+    checkedColor: false,
+    bgColor: '#f28b82'
+  },{
+    className: 'bg-orange',
+    checkedColor: false,
+    bgColor: '#fbbc04'
+  },{
+    className: 'bg-yellow',
+    checkedColor: false,
+    bgColor: '#fff475'
+  },{
+    className: 'bg-green',
+    checkedColor: false,
+    bgColor: '#ccff90'
+  },{
+    className: 'bg-oil-blue',
+    checkedColor: false,
+    bgColor: '#a7ffeb'
+  },{
+    className: 'bg-blue',
+    checkedColor: false,
+    bgColor: '#cbf0f8'
+  },{
+    className: 'bg-blue-dark',
+    checkedColor: false,
+    bgColor: '#aecbfa'
+  },{
+    className: 'bg-purple',
+    checkedColor: false,
+    bgColor: '#d7aefb'
+  },{
+    className: 'bg-rose',
+    checkedColor: false,
+    bgColor: '#fdcfe8'
+  },{
+    className: 'bg-brown',
+    checkedColor: false,
+    bgColor: '#e6c9a8'
+  },{
+    className: 'bg-grey',
+    checkedColor: false,
+    bgColor: '#e8eaed'
+  }
+]
--- front-end/form-keep/src/app/components/note-form/note-form.component.ts
+++ front-end/form-keep/src/app/components/note-form/note-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { colorPaletteBallsMock } from './mock/palletes-colors.mock'
+
 interface PalleteColors {
   className: string
   checkedColor: boolean
@@ -16,58 +18,7 @@ export class NoteFormComponent implements OnInit {
   noteToggle = false
   placeholder = 'Criar uma nota...'
   @Input() toggle: boolean
-
-  colorPaletteBalls = [
-    {
-      className: 'bg-default',
-      checkedColor: true,
-      bgColor: '#ffffffff',
-    },{
-      className: 'bg-red',
-      checkedColor: false,
-      bgColor: '#f28b82'
-    },{
-      className: 'bg-orange',
-      checkedColor: false,
-      bgColor: '#fbbc04'
-    },{
-      className: 'bg-yellow',
-      checkedColor: false,
-      bgColor: '#fff475'
-    },{
-      className: 'bg-green',
-      checkedColor: false,
-      bgColor: '#ccff90'
-    },{
-      className: 'bg-oil-blue',
-      checkedColor: false,
-      bgColor: '#a7ffeb'
-    },{
-      className: 'bg-blue',
-      checkedColor: false,
-      bgColor: '#cbf0f8'
-    },{
-      className: 'bg-blue-dark',
-      checkedColor: false,
-      bgColor: '#aecbfa'
-    },{
-      className: 'bg-purple',
-      checkedColor: false,
-      bgColor: '#d7aefb'
-    },{
-      className: 'bg-rose',
-      checkedColor: false,
-      bgColor: '#fdcfe8'
-    },{
-      className: 'bg-brown',
-      checkedColor: false,
-      bgColor: '#e6c9a8'
-    },{
-      className: 'bg-grey',
-      checkedColor: false,
-      bgColor: '#e8eaed'
-    }
-  ]
+  colorPaletteBalls = colorPaletteBallsMock
 
   constructor() { }
 
