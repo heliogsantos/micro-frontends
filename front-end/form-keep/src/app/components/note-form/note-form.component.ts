29db706b371278a4eb235f169464ac296d2b341b
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-07-16 18:12:15 -0300

    I created interface PalleteColors

--- front-end/form-keep/src/app/components/note-form/note-form.component.ts
+++ front-end/form-keep/src/app/components/note-form/note-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+interface PalleteColors {
+  className: string
+  checkedColor: boolean
+  bgColor: string
+}
 
 @Component({
   selector: 'app-note-form',
@@ -9,17 +15,82 @@ export class NoteFormComponent implements OnInit {
 
   noteToggle = false
   placeholder = 'Criar uma nota...'
+  @Input() toggle: boolean
+
+  colorPaletteBalls = [
+    {
+      className: 'bg-default',
+      checkedColor: true,
+      bgColor: '#ffffffff',
+    },{
+      className: 'bg-red',
+      checkedColor: false,
+      bgColor: '#f28b82'
+    },{
+      className: 'bg-orange',
+      checkedColor: false,
+      bgColor: '#fbbc04'
+    },{
+      className: 'bg-yellow',
+      checkedColor: false,
+      bgColor: '#fff475'
+    },{
+      className: 'bg-green',
+      checkedColor: false,
+      bgColor: '#ccff90'
+    },{
+      className: 'bg-oil-blue',
+      checkedColor: false,
+      bgColor: '#a7ffeb'
+    },{
+      className: 'bg-blue',
+      checkedColor: false,
+      bgColor: '#cbf0f8'
+    },{
+      className: 'bg-blue-dark',
+      checkedColor: false,
+      bgColor: '#aecbfa'
+    },{
+      className: 'bg-purple',
+      checkedColor: false,
+      bgColor: '#d7aefb'
+    },{
+      className: 'bg-rose',
+      checkedColor: false,
+      bgColor: '#fdcfe8'
+    },{
+      className: 'bg-brown',
+      checkedColor: false,
+      bgColor: '#e6c9a8'
+    },{
+      className: 'bg-grey',
+      checkedColor: false,
+      bgColor: '#e8eaed'
+    }
+  ]
 
   constructor() { }
 
-  openNote = () => {
+  openNote = event => {
+    event.stopPropagation()
     this.placeholder = 'Titulo'
-    this.noteToggle = !this.noteToggle
+    this.noteToggle = true
   }
 
   toogleNote = () => {
-    this.noteToggle = false
+    this.noteToggle = this.toggle
     this.placeholder = 'Criar uma nota...' 
+  }
+
+  addPallelaColor = (colorPaletteBall: PalleteColors) => {
+    this.removeCheckedPalleteColor()
+    colorPaletteBall.checkedColor = true
+  }
+
+  removeCheckedPalleteColor = () => {
+    this.colorPaletteBalls.forEach(colorPaletteBall => {
+      colorPaletteBall.checkedColor = false
+    })
   }
 
   ngOnInit(): void {
